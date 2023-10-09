ed2a3224e3e4fb29054308300afff424ccac0e68
Pritom Chandra Dey <133569468+Pritomkk@users.noreply.github.com>
2023-10-09 17:01:34 +0600

    Update Blood_Group.php

--- Lab 3.1/Task B/Blood_Group.php
+++ Lab 3.1/Task B/Blood_Group.php
@@ -1,3 +1,35 @@
+<html>
+
+<head>
+    <tittle> </tittle>
+</head>
+
+
+<body>
+        
+    <form method="post"action=""enctype="">
+
+        <fieldset>
+
+
+                <legend><h2>Blood_Group</h2></legend>
+                <select name="BG">
+                    <option value="">A+</option>            
+                    <option value="">B+</option>
+                    <option value="">A-</option>
+                    <option value="">B-</option>         
+                    <option value="">AB+</option> 
+                    <option value="">o+</option> 
+                    <option value="">o-</option>           
+                </select> <br> <br>
+
+                <input type="submit"name="Submit " value="Submit"/>
+        </fieldset>
+
+    </from>
+
+    
+
 <?php
 
 $Blood_Group=$_REQUEST['BG'];
@@ -6,3 +38,7 @@ echo $Blood_Group;
 
 
 ?>
+
+</body>
+
+</html>
